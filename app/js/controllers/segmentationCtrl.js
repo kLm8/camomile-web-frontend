479a4bdba39ed323c387e4fccc26b271335c96f0
Clement <gossart.clement@gmail.com>
2015-07-02 14:45:55 +0200

    tests loading audio

--- app/js/controllers/segmentationCtrl.js
+++ app/js/controllers/segmentationCtrl.js
@@ -549,17 +549,23 @@ angular.module('myApp.controllers')
 							var audioPath = camomileService.getMediumURL(data[0]._id, 'wav');
 							console.log('audio path: ' + audioPath); // "http://vmjoker:32772/medium/557ad06fff4a6b01002d64ab/wav"
 							
-							$http.get($sce.trustAsResourceUrl(audioPath)).
-								success(function(data, status, headers, config) {
-									// called asynchronously when response is available
-									console.log('Audio loaded');
-									console.log(data);
-									$scope.wavesurfer.load(data); 
-							}).
-								error(function(data, status, headers, config) {
-									// called asynchronously if error
-									console.log("Error loading audio");
-							});
+							// $http.get($sce.trustAsResourceUrl(audioPath)).
+							// 	success(function(data, status, headers, config) {
+							// 		// called asynchronously when response is available
+							// 		console.log('Audio loaded');
+							// 		console.log(data);
+							// 		$scope.wavesurfer.load(data); 
+							// }).
+							// 	error(function(data, status, headers, config) {
+							// 		// called asynchronously if error
+							// 		console.log("Error loading audio");
+							// });
+
+							var audio = $resource(audioPath);
+							console.log('audio : ' + audio);
+
+							$scope.wavesurfer.loadBlob(audio);
+							$scope.wavesurfer.load(audio);
 							
 							// "GET http://vmjoker:32772/medium/557ad06fff4a6b01002d64ab/wav 401 (Unauthorized)"
 
