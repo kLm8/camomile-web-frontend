d2616bc79430a828118574a09cdea0fe0b6fa7c2
Clement <gossart.clement@gmail.com>
2015-07-07 12:27:34 +0200

    improve: callbacks for navigation buttons in visjs timeline

--- app/js/controllers/segmentationCtrl.js
+++ app/js/controllers/segmentationCtrl.js
@@ -359,6 +359,34 @@ angular.module('myApp.controllers')
 				timechanged: $scope.onTimeChanged
 			};
 
+			/**
+			 * Move the timeline a given percentage to left or right
+			 * @param {Number} percentage   For example 0.1 (left) or -0.1 (right)
+			 */
+			$scope.zoom = function (percentage) {
+				var range = $scope.timeline.getWindow();
+				var interval = range.end - range.start;
+
+				$scope.timeline.setWindow({
+					start: range.start.valueOf() - interval * percentage,
+					end:   range.end.valueOf()   + interval * percentage
+				});
+			};
+
+			/**
+			 * Zoom the timeline a given percentage in or out
+			 * @param {Number} percentage   For example 0.1 (zoom out) or -0.1 (zoom in)
+			 */
+			$scope.move = function (percentage) {
+				var range = $scope.timeline.getWindow();
+				var interval = range.end - range.start;
+
+				$scope.timeline.setWindow({
+					start: range.start.valueOf() - interval * percentage,
+					end:   range.end.valueOf()   - interval * percentage
+				});
+			};
+
 		/*********************************************************************************************************/
 
 			var visjs2camomile = function(visjs) {
